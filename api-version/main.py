2154f6a3882769d63ae633fdba39f9f97632f9b4
Zixin_Xu <xzixin37@gmail.com>
2026-07-17 17:02:28 +1200

    Enable CORS

--- api-version/main.py
+++ api-version/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from database import get_connection
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Bill(BaseModel):
     type: str
